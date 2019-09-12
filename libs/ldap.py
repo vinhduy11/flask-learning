9ec7faca7ff43e1e9e37d795e126919f39ef4952
Dinh Vinh Duy <vinhduy11@gmail.com>
2019-09-12 17:55:52 +0700

    Fix bug LDAP

--- libs/ldap.py
+++ libs/ldap.py
@@ -9,18 +9,11 @@ class LDAP():
         self.LDAP_PASS = server_config.LDAP_PASS
         self.LDAP_TLS = server_config.LDAP_TLS
         self.connect = None
-        self.__create_connection()
-    
-    def __create_connection(self):
-        try:
-            self.connect = Connection(self.LDAP_SERVER, user='cn={user},dc=mservice,dc=org'.format(user=self.LDAP_USER), password=self.LDAP_PASS)
-        except Exception as e:
-            logger.error('[{}][{}]{}'.format(LDAP.__name__, self.__create_connection.__name__, e))
     
     def authen(self, username, password):
+        self.connect = None
         try:
-            user_dn = "cn={},dc=mservice,dc=org".format(username)
-            base_dn = "dc=mservice,dc=org"
+            
             server = Server(self.LDAP_SERVER, port=self.LDAP_PORT, use_ssl=False)
             self.connect = Connection(server, user='{}@mservice.org'.format(username), password=password, authentication='SIMPLE')
             logger.debug('[{}][{}]{}'.format(LDAP.__name__, self.authen.__name__, self.connect.bind()))
@@ -32,3 +25,6 @@ class LDAP():
                 return False
         except Exception as e:
             logger.error('[{}][{}]{}'.format(LDAP.__name__, self.authen.__name__, e))
+        finally:
+            if self.connect.bind():
+                self.connect.unbind()
